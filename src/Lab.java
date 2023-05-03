bf3725cf4e31ab7a126cf3f8f2923a287c850643
vidyaballa <vidyaballa503@gmail.com>
2023-05-04 01:14:02 +0530

    Hospital managemnt sysyem (object modeling)

---
+++ src/Lab.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+public class Lab {
+    String fecility;
+    int lab_cost;
+    void new_feci()
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.print("fecility:-");
+        fecility = input.nextLine();
+        System.out.print("cost:-");
+        lab_cost = input.nextInt();
+    }
+    void feci_list()
+    {
+        System.out.println(fecility + "\t\t" + lab_cost);
+    }
+}
